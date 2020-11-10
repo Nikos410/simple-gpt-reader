75183901ef2b91764893ec5e998741efc8f7de10
Nikos410 <nikos.epping@web.de>
2020-11-10 21:45:27 +0100

    initial import

---
+++ GuidPartitionTableHeader.hpp
@@ -0,0 +1,45 @@
+#include <stdint.h>
+
+struct GuidPartitionTableHeader {
+    // 8 bytes
+    char signature[8];
+
+    // 4 bytes
+    char revision[4];
+
+    // 4 bytes
+    uint32_t headerSize;
+
+    // 4 bytes
+    char crc32OfHeader[4];
+
+    // 4 bytes
+    char reserved[4];
+
+    // 8 bytes
+    uint64_t currentLba;
+
+    // 8 bytes
+    uint64_t backupLba;
+
+    // 8 bytes
+    uint64_t firstUsableLba;
+
+    // 8 bytes
+    uint64_t lastUsableLba;
+
+    // 16 bytes
+    char diskGuid[16];
+
+    // 8 bytes
+    uint64_t startingLbaOfArrayOfPartitionEntries;
+
+    // 4 bytes
+    uint32_t numberOfPartitionEntries;
+
+    // 4 bytes
+    uint32_t sizeOfASinglePartitionEntry;
+
+    // 4 bytes
+    char crc32OfPartitionEntryArray[4];
+};
---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <algorithm>
+#include "GuidPartitionTableHeader.hpp"
+
+const uint BLOCK_SIZE = 512;
+
+int main (int argc, char* argv[]) {
+    if (argc <= 1) {
+        std::cerr << "Please specify device" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    char* device = argv[1];
+    int fd = open(device, O_RDONLY | O_NONBLOCK);
+    if (fd < 0) {
+        std::cerr << "Could not open block device " << device << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // We want to read the second block (after the gpt header)
+    lseek(fd, BLOCK_SIZE, SEEK_SET);
+
+    char buffer[BLOCK_SIZE];
+    int bytesRead = read(fd, buffer, BLOCK_SIZE);
+
+    close(fd);
+
+    std::cout << bytesRead << " bytes read." << std::endl;
+
+    auto header = (GuidPartitionTableHeader*)buffer;
+    std::cout << std::endl << "### GPT Header ###" << std::endl;
+    std::cout << "Signature: " << header->signature << std::endl;
+
+    std::cout << "Revision: " ;
+    for (char i : header->revision) {
+        std::cout << (int)i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Header size: " << header->headerSize << std::endl;
+    std::cout << "Current LBA: " << header->currentLba << std::endl;
+    std::cout << "Backup LBA: " << header->backupLba << std::endl;
+    std::cout << "firstUsableLba: " << header->firstUsableLba << std::endl;
+    std::cout << "lastUsableLba: " << header->lastUsableLba << std::endl;
+
+    std::cout << "diskGuid: " ;
+    for (char i : header->diskGuid) {
+        printf("%hhx",i);
+    }
+    std::cout << std::endl;
+
+    std::cout << "startingLbaOfArrayOfPartitionEntries: " << header->startingLbaOfArrayOfPartitionEntries << std::endl;
+    std::cout << "numberOfPartitionEntries: " << header->numberOfPartitionEntries << std::endl;
+    std::cout << "sizeOfASinglePartitionEntry: " << header->sizeOfASinglePartitionEntry << std::endl;
+
+    return EXIT_SUCCESS;
+}
+
+
